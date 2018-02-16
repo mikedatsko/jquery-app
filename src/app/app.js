8af3bd77ac962049f16652d2d03439e26b856dbb
Mykhailo Datsko <mykhailo.datsko@solidopinion.com>
2018-02-16 08:48:03 +0200

    changed location to href

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,5 @@
 
 function App() {
-  window.parent.postMessage('FRAME_LOADED', (new URL(document.location)).searchParams.get('host_url') || 'http://jsmeasure.surge.sh');
+  window.parent.postMessage('FRAME_LOADED', (new URL(document.location.href)).searchParams.get('host_url') || 'http://jsmeasure.surge.sh');
   listTodos.getList();
 }
